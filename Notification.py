9baceca4b282be5d39757a45810ab7a2ec5c2379
adhyay2000 <adityabundi2000@gmail.com>
2020-12-31 13:47:32 +0530

    Added the requirements.txt file and shifted all the changes to Notification.py file

--- Notification.py
+++ Notification.py
@@ -1,14 +1,13 @@
 from win10toast import ToastNotifier
-import os
+import os,platform
 
 
 class Notification:
     def __init__(self, trigger=None, CompanyName: str = ""):
-        self.toast = ToastNotifier()
-        self.toast.show_toast(trigger.name, f"{CompanyName}'s has been triggered, Price {trigger.relationName} {trigger.value}")
-
-class Notification_linux:
-    def __init__(self, trigger=None, CompanyName: str = ""):
-        title = "Stock Trigger"
-        message = f"{CompanyName}'s has been triggered, Price {trigger.relationName} {trigger.value}"
-        os.system('notify-send "{}" "{}"'.format(title,message))
+        if platform.system() == 'Windows':
+            self.toast = ToastNotifier()
+            self.toast.show_toast(trigger.name, f"{CompanyName}'s has been triggered, Price {trigger.relationName} {trigger.value}")
+        elif platform.system() == 'Linux':
+            title = "Stock Trigger"
+            message = f"{CompanyName}'s has been triggered, Price {trigger.relationName} {trigger.value}"
+            os.system('notify-send "{}" "{}"'.format(title,message))
